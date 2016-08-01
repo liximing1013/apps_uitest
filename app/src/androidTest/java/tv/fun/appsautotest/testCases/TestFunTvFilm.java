b69145348796f0ad8ab21d147de49a4d452f1d35
Vieira-zj <zhengjin@fun.tv>
2016-08-01 18:15:09 +0800

    1. Remove temp files; 2. Finish test demo testPlayFilm.

--- app/src/androidTest/java/tv/fun/appsautotest/testCases/TestFunTvFilm.java
+++ app/src/androidTest/java/tv/fun/appsautotest/testCases/TestFunTvFilm.java
@@ -5,12 +5,16 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.Until;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 /**
  * Created by zhengjin on 2016/7/28.
@@ -22,18 +26,74 @@ public class TestFunTvFilm {
 
     private UiDevice mDevice;
 
+    @Before
+    public void setUp() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+    }
+
     @Test
     public void testPlayFilm() {
-
         // back to launcher home
-        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-
         mDevice.pressHome();
+        mDevice.waitForIdle();
         SystemClock.sleep(3000);
 
-        mDevice.waitForIdle();
         boolean ret = mDevice.wait(Until.hasObject(By.pkg("com.bestv.ott").depth(0)), 8000);
         Assert.assertTrue("Verify back to home", ret);
+
+        // open film page
+        List<UiObject2> tabsText = mDevice.findObjects(By.res("com.bestv.ott:id/title"));
+        Assert.assertTrue(tabsText.size() > 0);
+
+        UiObject2 filmTabText = null;
+        for (UiObject2 text : tabsText) {
+            if ("电影".equals(text.getText())) {
+                filmTabText = text;
+            }
+        }
+        Assert.assertNotNull(filmTabText);
+
+        filmTabText.getParent().click();
+        SystemClock.sleep(1000);
+        mDevice.pressEnter();
+        mDevice.waitForIdle();
+        SystemClock.sleep(5000);
+
+        // select a film, and open film details page
+        mDevice.pressDPadLeft();
+        mDevice.waitForIdle();
+        SystemClock.sleep(3000);
+
+        mDevice.pressDPadDown();
+        SystemClock.sleep(1000);
+        mDevice.pressDPadLeft();
+        SystemClock.sleep(1000);
+
+        mDevice.pressEnter();
+        mDevice.waitForIdle();
+        SystemClock.sleep(3000);
+
+        UiObject2 filmTitle = mDevice.findObject(By.res("com.bestv.ott:id/detail_title"));
+        String filmName = filmTitle.getText();
+        Assert.assertFalse(filmName == null || "".equals(filmName));
+
+        // play film
+        mDevice.pressEnter();
+        SystemClock.sleep(8000);
+        ret = mDevice.wait(Until.hasObject(By.clazz(
+                "com.funshion.player.play.funshionplayer.VideoViewPlayer")), (15 * 1000));
+        Assert.assertTrue(ret);
+
+        // wait 10 seconds and pause
+        mDevice.pressEnter();
+        SystemClock.sleep(1000);
+
+        String filmNameInPlayer =
+                mDevice.findObject(By.res("com.bestv.ott:id/video_player_title")).getText();
+        Assert.assertEquals(filmName.trim(), filmNameInPlayer.trim());
+
+        ret = mDevice.wait(Until.hasObject(By.res("com.bestv.ott:id/video_player_time")), 1000);
+        Assert.assertTrue(ret);
     }
 
 }
