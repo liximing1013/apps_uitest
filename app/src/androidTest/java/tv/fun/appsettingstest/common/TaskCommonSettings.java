bae33124da8afbbb0f32b3da64dd4334dd9e8b40
Vieira-zj <zhengjin@fun.tv>
2016-10-25 12:02:55 +0800

    Init for common settings test.

---
+++ app/src/androidTest/java/tv/fun/appsettingstest/common/TaskCommonSettings.java
@@ -0,0 +1,10 @@
+package tv.fun.appsettingstest.common;
+
+/**
+ * Created by zhengjin on 2016/10/25.
+ *
+ * TaskCommonSettings
+ */
+
+public final class TaskCommonSettings {
+}
---
+++ app/src/androidTest/java/tv/fun/appsettingstest/testCases/TestCommonSettings.java
@@ -0,0 +1,8 @@
+package tv.fun.appsettingstest.testCases;
+
+/**
+ * Created by zhengjin on 2016/10/25.
+ */
+
+public final class TestCommonSettings {
+}
