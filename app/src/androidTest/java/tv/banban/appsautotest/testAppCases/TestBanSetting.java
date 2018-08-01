431ffc0155beb3e86ae2f7ac5313709bb329a496
liximing1013 <973878442@qq.com>
2018-08-01 17:26:21 +0800

    上传本地代码到github

--- app/src/androidTest/java/tv/banban/appsautotest/testAppCases/TestBanSetting.java
+++ app/src/androidTest/java/tv/banban/appsautotest/testAppCases/TestBanSetting.java
@@ -10,6 +10,7 @@ import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
 import android.util.Log;
@@ -68,15 +69,39 @@ public class TestBanSetting extends CommonMethod {
             stopApp();  //adb shell dumpsys window w |findstr \/ |findstr name=
             startApp(x86_PACKNAME, x86_CLASSNAME); //启动x86模拟器
             appIsRunning();
-            if("com.ee.xianshi.android".equals(uiDevice.getCurrentPackageName())){
+            if(x86_PACKNAME.equals(uiDevice.getCurrentPackageName())){
                 clickByTextAndClazz("我","android.widget.TextView"); //1s
                 clickByCoordinate(433,112,2); //2s
                 ScreenShot("test_settings_01.png"); //storage/emulated/0/Android/data/tv.fun.appsautotest/cache
                 UiObject2 page = uiDevice.findObject(By.text("个人资料").clazz("android.widget.TextView"));
                 m_Actual = page.getText(); //实际结果
                 m_Expect = "个人资料"; //预期结果
-                Assert.assertEquals("跳转个人资料页面错误",m_Expect,m_Actual);
+                Assert.assertEquals("跳转个人资料页面错误",m_Expect,m_Actual); //断言
+                UiScrollable scroll = new UiScrollable(new UiSelector().className("android.widget.ScrollView"));
+                UiSelector gift = new UiSelector().text("收到的礼物");
+                scroll.scrollIntoView(gift);
+                UiObject2 giftTotal = uiDevice.findObject(By.textContains("520"));
+                m_Actual = giftTotal.getText();
+                m_Expect = "520";
+                Utils.writeCaseResult("礼物墙模块错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
             }
+//            if(ANDROID_PACKNAME.equals(uiDevice.getCurrentPackageName())){
+//                clickByTextAndClazz("我","android.widget.TextView");
+//                clickByCoordinate(433,112,2);
+//                ScreenShot("test_settings_01.png");
+//                UiObject2 page = uiDevice.findObject(By.text("个人资料").clazz("android.widget.TextView"));
+//                m_Actual = page.getText();
+//                m_Expect = "个人资料";
+//                Assert.assertEquals("跳转个人资料页面错误",m_Expect,m_Actual);
+//                UiScrollable scroll = new UiScrollable(new UiSelector().className("android.widget.ScrollView"));
+//                UiSelector gift = new UiSelector().text("收到的礼物");
+//                scroll.scrollIntoView(gift);
+//                UiObject2 giftTotal = uiDevice.findObject(By.textContains("520"));
+//                m_Actual = giftTotal.getText();
+//                Log.i(TAG, "test_settings_01_MyPagePersonInformation: "+m_Actual);
+//                m_Expect = "520";
+//                Utils.writeCaseResult("礼物墙模块错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+//            }
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -93,8 +118,8 @@ public class TestBanSetting extends CommonMethod {
         try {
             clickByTextAndClazz("交友动态","android.widget.TextView");
             ScreenShot("test_settings_02.png");
-            UiObject2 page = uiDevice.findObject(By.text("标签"));
-            m_Actual = page.getText();
+            UiObject2 label = uiDevice.findObject(By.text("标签"));
+            m_Actual = label.getText();
             m_Expect = "标签";
             Utils.writeCaseResult("跳转交友动态页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
         }catch (Exception e){
@@ -113,8 +138,8 @@ public class TestBanSetting extends CommonMethod {
         try {
             clickByTextAndClazz("活动礼包","android.widget.TextView");
             ScreenShot("test_settings_03.png");
-            UiObject2 page = uiDevice.findObject(By.text("活动"));
-            m_Actual = page.getText();
+            UiObject2 activityPage = uiDevice.findObject(By.text("活动"));
+            m_Actual = activityPage.getText();
             m_Expect = "活动";
             Utils.writeCaseResult("跳转活动页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
         }catch (Exception e){
@@ -136,7 +161,7 @@ public class TestBanSetting extends CommonMethod {
             UiObject2 page = uiDevice.findObject(By.text("立即充值").clazz("android.widget.TextView"));
             m_Actual = page.getText();
             m_Expect = "立即充值";
-            Utils.writeCaseResult("跳转活动页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+            Utils.writeCaseResult("跳转钱包页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -153,10 +178,15 @@ public class TestBanSetting extends CommonMethod {
         try {
             clickByTextAndClazz("会员","android.widget.TextView");
             ScreenShot("test_settings_05.png");
-            UiObject2 page = uiDevice.findObject(By.text("贵族身份").clazz("android.widget.TextView"));
-            m_Actual = page.getText();
-            m_Expect = "贵族身份";
-            Utils.writeCaseResult("跳转会员页面错误",m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+            // 贵族身份
+            UiObject2 page1 = uiDevice.findObject(By.text("贵族身份"));
+            Assert.assertEquals("跳转会员页面错误","贵族身份",page1.getText());
+            // 大厅Vip
+            UiObject2 page2 = uiDevice.findObject(By.text("大厅VIP"));
+            Assert.assertEquals("跳转会员页面错误","大厅VIP",page2.getText());
+            // 我的礼包
+            UiObject2 page3 = uiDevice.findObject(By.text("我的礼包"));
+            Assert.assertEquals("跳转会员页面错误","我的礼包",page3.getText());
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -171,12 +201,15 @@ public class TestBanSetting extends CommonMethod {
     @Test //邀约页面
     public void test_settings_06_InviteAndOrderPage(){
         try {
-            clickByTextAndClazz("邀约","android.widget.TextView");
-            ScreenShot("test_settings_06.png");
-            UiObject2 page = uiDevice.findObject(By.text("接单记录").clazz("android.widget.TextView"));
-            m_Actual = page.getText();
-            m_Expect = "接单记录";
-            Utils.writeCaseResult("跳转邀约页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+            UiObject2 auth = uiDevice.findObject(By.text("资质认证"));
+            if(auth == null){
+                clickByTextAndClazz("邀约","android.widget.TextView");
+                ScreenShot("test_settings_06.png");
+                UiObject2 page = uiDevice.findObject(By.text("接单记录").clazz("android.widget.TextView"));
+                m_Actual = page.getText();
+                m_Expect = "接单记录";
+                Utils.writeCaseResult("跳转邀约页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+            }
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -193,10 +226,30 @@ public class TestBanSetting extends CommonMethod {
         try {
             clickByTextAndClazz("资质","android.widget.TextView");
             ScreenShot("test_settings_07.png");
-            UiObject2 page = uiDevice.findObject(By.text("资质设置").clazz("android.widget.TextView"));
-            m_Actual = page.getText();
-            m_Expect = "资质设置";
-            Utils.writeCaseResult("跳转资质页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+            UiObject2 skillPage = uiDevice.findObject(By.text("资质设置"));
+            Assert.assertEquals("跳转资质页面错误","资质设置",skillPage.getText());
+            UiScrollable scroll = new UiScrollable(new UiSelector().className("android.widget.ScrollView"));
+            scroll.flingForward();
+            systemWait(SHORT_WAIT);
+            UiObject2 newSkill = uiDevice.findObject(By.text("新资质"));
+            if(newSkill != null){
+                clickByTextAndClazz("新资质","android.widget.TextView");
+                ScreenShot("test_settings_07_1.png");
+                UiObject2 newSkillPage = uiDevice.findObject(By.text("已有资质").clazz("android.widget.TextView"));
+                m_Actual = newSkillPage.getText();
+                m_Expect = "已有资质";
+                Utils.writeCaseResult("跳转申请资质页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+                UiObject2 LOl = uiDevice.findObject(By.text("LOL"));
+                if(LOl != null){
+                    clickByTextAndClazz("LOL","android.widget.TextView");
+                    ScreenShot("test_settings_07_2.png");
+                    UiObject2 LolSkillPage = uiDevice.findObject(By.text("LOL资质").clazz("android.widget.TextView"));
+                    m_Actual = LolSkillPage.getText();
+                    m_Expect = "LOL资质";
+                    Utils.writeCaseResult("跳转申请资质页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+                }
+                pressBack(2);
+            }
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -213,10 +266,28 @@ public class TestBanSetting extends CommonMethod {
         try {
             clickByTextAndClazz("账号设置","android.widget.TextView");
             ScreenShot("test_settings_08.png");
-            UiObject2 page = uiDevice.findObject(By.text("手机号").clazz("android.widget.TextView"));
-            m_Actual = page.getText();
-            m_Expect = "手机号";
-            Utils.writeCaseResult("跳转账号设置页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+            UiObject2 accountPage = uiDevice.findObject(By.text("手机号").clazz("android.widget.TextView"));
+            Assert.assertEquals("跳转账号设置页面错误","手机号",accountPage.getText());
+            UiObject2 cashMoney = uiDevice.findObject(By.text("提现账号"));
+            if(cashMoney != null){
+                clickByTextAndClazz("提现账号","android.widget.TextView");
+                ScreenShot("test_settings_08_1.png");
+                UiObject2 cashPage = uiDevice.findObject(By.text("提现到银行卡/支付宝"));
+                m_Actual = cashPage.getText();
+                m_Expect = "提现到银行卡/支付宝";
+                Utils.writeCaseResult("跳转提现页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+                pressBack(1);
+            }
+            UiObject2 Hot = uiDevice.findObject(By.text("红人认证"));
+            if(Hot != null){
+                clickByTextAndClazz("红人认证","android.widget.TextView");
+                ScreenShot("test_settings_08_2.png");
+                UiObject2 cashPage = uiDevice.findObject(By.text("通过红人认证后您将享有以下特权"));
+                m_Actual = cashPage.getText();
+                m_Expect = "通过红人认证后您将享有以下特权";
+                Utils.writeCaseResult("跳转红人认证页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
+                pressBack(1);
+            }
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -296,6 +367,7 @@ public class TestBanSetting extends CommonMethod {
             UiObject2 page = uiDevice.findObject(By.text("伴伴").clazz("android.widget.TextView"));
             m_Actual = page.getText();
             m_Expect = "伴伴";
+            Utils.writeCaseResult("跳转到关于我们页面错误", m_Actual.equalsIgnoreCase(m_Expect), m_Time);
         }catch (Exception e){
             e.printStackTrace();
             resultStr = e.toString();
@@ -405,33 +477,6 @@ public class TestBanSetting extends CommonMethod {
             }
             pressBack(1);
         }
-    }
-
-    @Test //
-    public void test_settings_16_HelpDocsClassification(){
-
-    }
-
-    private void banBan(){
-        UiObject2 gc = uiDevice.findObject(By.text("广场"));
-        if(gc != null){
-            UiObject2 me = uiDevice.findObject(By.text("我").clazz("android.widget.TextView"));
-            me.click();
-            systemWait(1);
-            UiObject2 gcc = uiDevice.findObject(By.text("交友动态"));
-            gcc.clickAndWait(Until.newWindow(), 3000);
-        }
-    }
-
-    private void stopBanBan() throws RemoteException,UiObjectNotFoundException {
-        uiDevice.pressRecentApps();
-        UiObject recentapp = new UiObject(new UiSelector().resourceId("com.android.systemui:id/dismiss_task"));
-        do{
-            recentapp.waitForExists(2000);
-            if(recentapp.exists()){
-                recentapp.swipeLeft(5);
-            }
-        }while(recentapp.exists());
     }
 
 }
